578651664543420a4acd53970ffcafa45fd170f7
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-06-20 22:30:20 +0530

    buf fix underway - safari crash

--- application/views/header.php
+++ application/views/header.php
@@ -14,12 +14,12 @@
     
     <!-- Javascript calls
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-
-<!--     <script type="text/javascript" src="<?=PUBLIC_URL?>js/viewer.js"></script>
-    <script type="text/javascript" src="<?=PUBLIC_URL?>js/bootstrap-datepicker.min.js"></script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> -->
+    <!-- <script type="text/javascript" src="<?=PUBLIC_URL?>js/vanillaCalendar.js"></script> -->
+    <script type="text/javascript" src="<?=PUBLIC_URL?>js/viewer.js"></script>
+    <script type="text/javascript" src="<?=PUBLIC_URL?>js/bootstrap-datepicker.min.js"></script>
 
 	<!-- CSS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
