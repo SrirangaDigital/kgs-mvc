65ed5a0418d95ed79abaf00963e93d7b06aa1603
root <root@localhost.localdomain>
2018-06-22 18:24:56 +0530

    Config from server

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://localhost/kgs-mvc/');
+define('BASE_URL', 'http://kgssharma.in/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('DATA_URL', PUBLIC_URL . 'data/');
 define('METADATA_URL', PUBLIC_URL . 'metaData/');
@@ -8,7 +8,7 @@ define('FLAT_URL', BASE_URL . 'application/views/flat/');
 define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 
 // Physical location of resources
-define('PHY_BASE_URL', '/var/www/html/kgs-mvc/');
+define('PHY_BASE_URL', '/var/www/html/kgssharma.in/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_DATA_URL', PHY_PUBLIC_URL . 'data/');
 define('PHY_METADATA_URL', PHY_PUBLIC_URL . 'metaData/');
@@ -20,7 +20,7 @@ define('DB_PREFIX', 'kgs');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hits');
 define('kgsHITS_USER', 'root');
-define('kgsHITS_PASSWORD', 'mysql');
+define('kgsHITS_PASSWORD', 'Sriranga@#$!');
 
 // Git config
 define('GIT_USER_NAME', 'shruthisdst');
