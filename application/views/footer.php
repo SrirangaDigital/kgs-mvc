964c7b7615a1df083e6331cedba68f3736939d16
suresh07 <soori_rozee@yahoo.com>
2018-06-22 18:03:26 +0530

    Footer ui fixes

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,22 +1,24 @@
     <!-- Footer -->
     <div class="container">
         <div class="row  justify-content-end">
-            <div class="visitorCount">
-                Visitors<br />
-                <strong><?=$_SESSION['hits'];?></strong>
-            </div>
+            
         </div>
     </div>
     <div id="k-subfooter">
-        <div class="container-fluid"><!-- container -->
-            <div class="row"><!-- row -->
-                <div class="col-lg-12">
-                    <p class="copy-text text-center"><small>&copy; 2018 ವಿದ್ವಾನ್ ಡಾ ॥ ಕೆ. ಜಿ. ಸುಬ್ರಾಯಶರ್ಮಾ, ಬೆಂಗಳೂರು. ಎಲ್ಲ ಹಕ್ಕುಗಳನ್ನು ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ.</small></p>
+        <div class="container"><!-- container -->
+            <div class="row justify-content-center"><!-- row -->
+                <div class="col-md-12">
+                    <p class="copy-text text-center">
+                        <small>&copy; 2018 ವಿದ್ವಾನ್ ಡಾ ॥ ಕೆ. ಜಿ. ಸುಬ್ರಾಯಶರ್ಮಾ, ಬೆಂಗಳೂರು. ಎಲ್ಲ ಹಕ್ಕುಗಳನ್ನು ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ.</small>
+                        <span class="visitorCount">
+                            Visitors: 
+                            <strong><?=$_SESSION['hits'];?></strong>
+                        </span>
+                    </p>
                 </div>
             </div><!-- row end -->
         </div><!-- container end -->
     </div>
-
     <!-- End Main page layout
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <script type="text/javascript" src="<?=PUBLIC_URL?>js/common.js?v=1.1" async></script>
