0c56ab73eae17ed6533b9193932651f3194e2dd9
Suresh <soori_rozee@yahoo.com>
2019-03-09 15:47:07 +0530

    Local Credentials added

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://kgssharma.in/');
+define('BASE_URL', 'http://192.168.1.101/kgs-mvc/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('DATA_URL', PUBLIC_URL . 'data/');
 define('METADATA_URL', PUBLIC_URL . 'metaData/');
@@ -8,7 +8,7 @@ define('FLAT_URL', BASE_URL . 'application/views/flat/');
 define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 
 // Physical location of resources
-define('PHY_BASE_URL', '/var/www/html/kgssharma.in/');
+define('PHY_BASE_URL', '/var/www/html/kgs-mvc/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_DATA_URL', PHY_PUBLIC_URL . 'data/');
 define('PHY_METADATA_URL', PHY_PUBLIC_URL . 'metaData/');
@@ -20,13 +20,5 @@ define('DB_PREFIX', 'kgs');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'hits');
 define('kgsHITS_USER', 'root');
-define('kgsHITS_PASSWORD', 'Sriranga@#$!');
-
-// Git config
-define('GIT_USER_NAME', 'shruthisdst');
-define('GIT_PASSWORD', 'shruthitr14');
-define('GIT_REPO', 'github.com/SrirangaDigital/jss-nosql.git');
-define('GIT_REMOTE', 'https://' . GIT_USER_NAME . ':' . GIT_PASSWORD . '@' . GIT_REPO);
-define('GIT_EMAIL', 'shruthitr.nayak@gmail.com');
-
+define('kgsHITS_PASSWORD', 'mysql');
 ?>
